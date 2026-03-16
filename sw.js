3176bbd84279e2088700cb7e5e167f4434cf18c5
tomjack9527 <83287388+tomjack9527@users.noreply.github.com>
2026-03-16 16:51:52 +0800

    update

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'xiaohongshu-v1';
+const BASE_PATH = '/xiaohongshu-title-generator/';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
